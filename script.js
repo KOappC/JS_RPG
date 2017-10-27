7a81234e9ec0566a0229d3cae50476fb143ab159
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-10-27 10:02:15 -0400

    refined console dialog, and logic

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 var health = 40;
 var grant = 10;
 var lives = 0;
-var play = prompt("Are you ready to rumble?");
-play = play.toLowerCase();
+//var cont = true;
+var play = prompt("Are you ready to rumble?").toLowerCase();
 while (play !== "yes") {
-    play = prompt("How about ice cream?");
+    play = prompt("How about ice cream?").toLowerCase();
+    if (play !== "yes") {
+        alert("Yeah, about that.  There isn't any ice cream...");
+    }
     alert("Tricked you!  Round 1")
     break;
 }
-
 var name = prompt("What's your name");
 if (name === "") {
     name = "Player";
 }
 
 
-while (health >= 0 && lives <= 3) {
+
+while (cont = true) {
     health -= Math.floor((Math.random() * 2) + 1);
     grant -= Math.floor((Math.random() * 2) + 1);
+    console.log("Grant pecked " + name + ". You have " + health + " health remaining.");
+    console.log("You hit Grant! They have " + grant + " health remaining!");
 
-    console.log(name + ", you have " + health + " health, and Grant has " + grant + " health left.  You currently have been undefeated for " + lives + " rounds!");
     if (grant <= 0) {
         grant = 10;
         lives += 1;
+        console.log("Round " + lives + " won!")
     } else if (lives === 3) {
         console.log("Congratulations, " + name + "! Grant is no more!");
         break;
     } else if (health <= 0) {
-        console.log("Grant owned you...better luck next time")
+        cont = false;
+        console.log("Grant owned you...better luck next time");
         break;
     }
 }
 
 /*  need to:
         - stop the game from playing the start of the final round
+        - allow the user to avoid playing
+                - and skip the name prompt
 
     want to:
-        - don't count grant's win as player's
         - keep health and grant positive
-
+        -
 
  */
