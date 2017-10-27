742d6cbf52de4c382a93a29e80c4c833d5b3cceb
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-10-27 14:28:05 -0400

    added comments on requirements for part 2

--- script.js
+++ script.js
@@ -38,7 +38,11 @@ while (cont = true) {
     }
 }
 
-/*  need to:
+/*
+
+    Things that i think:
+
+    need to:
         - stop the game from playing the start of the final round
         - allow the user to avoid playing
                 - and skip the name prompt
@@ -46,5 +50,24 @@ while (cont = true) {
     want to:
         - keep health and grant positive
         -
+
+
+    part 2 requirements:
+
+        1. startGame function. This function will
+          a. Prompt the user if they would like to play, and if they say yes...
+                i. Allow the user to enter the character’s name.
+                ii. Call the startCombat function.
+
+        2. startCombat function. When executed, this will run the entire loop you created in Part One.
+
+        3. getDamage function. This will return a number between 1 and 5 that will be used to decide how much damage the user and Grant will deal each round.
+
+        4. Each iteration of the loop will include a prompt that will ask the user if they would like to “attack” or “quit”.
+
+        5. If the user decides to attack, adjust the character’s health points and Grant’s health points based on the getDamage function.
+
+        6. If the user decides to quit, figure out a way to exit out of the loop and function.
+
 
  */
