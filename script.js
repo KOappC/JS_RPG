42582e10f082b8f36526e7eada84f7c525017d5e
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-10-27 15:33:12 -0400

    completed steps 1-3 in part 2 requirments

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
 var health = 40;
 var grant = 10;
 var lives = 0;
-//var cont = true;
-var play = prompt("Are you ready to rumble?").toLowerCase();
-while (play !== "yes") {
-    play = prompt("How about ice cream?").toLowerCase();
-    if (play !== "yes") {
-        alert("Yeah, about that.  There isn't any ice cream...");
+
+
+
+function startGame() {
+    var play = prompt("Would you like to play?").toLowerCase();
+    if (play === "yes"){
+        var name = prompt("What's your name");
+        startCombat();
+    } else if (play !== "yes"){
+        console.log("Have a great day");
     }
-    alert("Tricked you!  Round 1")
-    break;
 }
-var name = prompt("What's your name");
+
 if (name === "") {
     name = "Player";
 }
 
+function getDamage(player, enemy) {
+    health -= Math.floor((Math.random() * 5) + 1);
+    grant -= Math.floor((Math.random() * 5) + 1);
+}
 
+function byeBye() {
+    console.log("Bye, bye");
+}
 
-while (cont = true) {
-    health -= Math.floor((Math.random() * 2) + 1);
-    grant -= Math.floor((Math.random() * 2) + 1);
-    console.log("Grant pecked " + name + ". You have " + health + " health remaining.");
-    console.log("You hit Grant! They have " + grant + " health remaining!");
+function startCombat() {
+    while (cont = true) {
+        var turn = prompt("Would you like to attack or chicken out?").toLowerCase();
+        while (turn === "attack") {
+            break;
+        } if (turn !== "attack") {
+            byeBye();
+            break;
+        }
+        getDamage(health, grant);
+        console.log("Grant pecked " + name + ". You have " + health + " health remaining.");
+        console.log("You hit Grant! They have " + grant + " health remaining!");
 
-    if (grant <= 0) {
-        grant = 10;
-        lives += 1;
-        console.log("Round " + lives + " won!")
-    } else if (lives === 3) {
-        console.log("Congratulations, " + name + "! Grant is no more!");
-        break;
-    } else if (health <= 0) {
-        cont = false;
-        console.log("Grant owned you...better luck next time");
-        break;
+        if (grant <= 0) {
+            grant = 10;
+            lives += 1;
+            console.log("Round " + lives + " won!")
+        } else if (lives === 3) {
+            console.log("Congratulations, " + name + "! Grant is no more!");
+            break;
+        } else if (health <= 0) {
+            cont = false;
+            console.log("Grant owned you...better luck next time");
+            break;
+        }
     }
 }
+
+startGame();
+
+
+
+// List of objectives
 
 /*
 
@@ -54,14 +77,14 @@ while (cont = true) {
 
     part 2 requirements:
 
-        1. startGame function. This function will
-          a. Prompt the user if they would like to play, and if they say yes...
-                i. Allow the user to enter the character’s name.
-                ii. Call the startCombat function.
+        //DONE  1. startGame function. This function will
+                    a. Prompt the user if they would like to play, and if they say yes...
+                        i. Allow the user to enter the character’s name.
+                        ii. Call the startCombat function.
 
-        2. startCombat function. When executed, this will run the entire loop you created in Part One.
+        //DONE  2. startCombat function. When executed, this will run the entire loop you created in Part One.
 
-        3. getDamage function. This will return a number between 1 and 5 that will be used to decide how much damage the user and Grant will deal each round.
+        //DONE  3. getDamage function. This will return a number between 1 and 5 that will be used to decide how much damage the user and Grant will deal each round.
 
         4. Each iteration of the loop will include a prompt that will ask the user if they would like to “attack” or “quit”.
 
@@ -71,3 +94,22 @@ while (cont = true) {
 
 
  */
+
+
+
+// Bad jokes from the first iteration of this project
+
+/*
+
+var play = prompt("Are you ready to rumble?").toLowerCase();
+while (play !== "yes") {
+    play = prompt("How about ice cream?").toLowerCase();
+    if (play !== "yes") {
+        alert("Yeah, about that.  There isn't any ice cream...");
+    }
+    alert("Tricked you!  Round 1")
+    break;
+}
+var name = prompt("What's your name");
+
+*/
