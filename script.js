522e48cfe5d9f22e91ba2c47108f76ad5276c1b2
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-10-31 17:11:02 -0400

    created objects for grant and player, reworked main while loop

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 startGame();
 
-function getDamage() {
-    return Math.floor((Math.random() * 5) + 1);
+var player = {
+    wins: 0,
+    health: 40,
+    healsRemaining: 2,
+    name: prompt("What are we going to call your competitor?"),
+    getDamage() {
+        return Math.floor((Math.random() * 3) + 1);
+    },
+    heal(healsRemaining) {
+        this.healsRemaining = healsRemaining;
+        return Math.floor((Math.random() * 10) + 1);
+    }
+};
+
+var grant = {
+    grant: 10,
+    name: prompt("Who is your nemesis? (keep in mind this is a chicken)"),
+    getDamage() {
+        return Math.floor((Math.random() * 3) + 1);
+    }
+
 }
+
+
+function startGame() {
+    var play = prompt("Chicken fight time!  Are you ready?").toLowerCase();
+    if (play === "yes"){
+        startCombat()
+    } else if (play !== "yes"){
+        console.log("You know where to find me, if you change your mind.");
+    }
+}
+
+// lab part 2
+/*
 
 function startGame() {
     var play = prompt("Are you ready to rumble?!").toLowerCase();
@@ -13,40 +45,44 @@ function startGame() {
         console.log("You know where to find me, if you change your mind.");
     }
 }
+*/
 
-function startCombat(name) {
-    var health = 40;
-    var grant = 10;
-    var wins = 0;
+function startCombat(player, grant) {
+
 
     while (cont = true) {
-        if (wins === 3) {
-            console.log("Congratulations, " + name + "! Grant is no more!");
+        if (player.wins === 3) {
+            console.log("Congratulations, " + player.name + "! " + grant.name + " is no more!");
             break;
         }
 
-        var nextRound = prompt("Would you like to attack or chicken out?").toLowerCase();
+        var nextRound = prompt("Would you like to attack, heal or chicken out?").toLowerCase();
 
         if (nextRound === "attack") {
-            health -= getDamage();
-            grant -= getDamage();
+            player -= player.getDamage();
+            grant -= grant.getDamage();
 
-            console.log("Grant pecked " + name + ". You have " + health + " health remaining.");
-            console.log("You hit Grant! They have " + grant + " health remaining!");
+            console.log( " pecked " + name + ". You have " + player.health + " health remaining.");
+            console.log("You hit " + grant.name +"! They have " + grant.grant + " health remaining!");
         } else if (nextRound !== "attack") {
             console.log("You were doing so well, though.");
             return;
         }
 
-        if (grant <= 0) {
-            grant = 10;
-            wins += 1;
+        if (nextRound === "heal" && player.healsRemaining > 0) {
+            player.health += player.heal();
+            player.healsRemaining -= 1;
+        }
+
+        if (grant.grant <= 0) {
+            grant.grant = 10;
+            player.wins += 1;
             console.log("Round " + wins + " won!");
         }
 
-        if (health <= 0) {
+        if (player.health <= 0) {
             cont = false;
-            console.log("Grant owned you...better luck next time");
+            console.log(grant.name + " owned you...better luck next time");
             break;
         }
     }
@@ -60,13 +96,15 @@ function startCombat(name) {
 
     Things that i think:
 
+        need to:
+            - "get to" the player and grant objects in startGame()
 
-    want to:
-        - create default player name
+        want to:
+            - create default player name
 
     part 3 requirements:
 
-        1. Each time the user is prompted, they ahve the choice either to attack, heal or quit.  Grant always attacks.
+        1. Each time the user is prompted, they have the choice either to attack, heal or quit.  Grant always attacks.
         2. Do not allow heal if there are no heals remaining.
         3. Grant still regenerates to 10 when his health hits 0.  Every time this happens, add 1 to the user
             character's wins.
