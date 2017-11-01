6cb966ab62d160aa6e2ace03d9770e6da5bf8d68
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-01 16:13:27 -0400

    prepared for part 4

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 startGame();
 
-var player = {
-    name: "",
-    wins: 0,
-    health: 40,
-    healsRemaining: 2,
-    getDamage() {
-        return Math.floor((Math.random() * 3) + 1);
-    },
-    heal(healsRemaining) {
-        this.healsRemaining = healsRemaining;
-        return Math.floor((Math.random() * 10) + 1);
-    }
-};
-
-var grant = {
-    name: "",
-    grant: 10,
-    getDamage() {
-        return Math.floor((Math.random() * 3) + 1);
-    }
-
-};
-
 
 function startGame() {
+    
     var play = prompt("Chicken fight time!  Are you ready?").toLowerCase();
+
     if (play === "yes"){
-        player.name = prompt("What are we going to call you competitor? (This is your chicken)");
-        grant.name = prompt("Who is your nemesis? (Keep in mind, this is a chicken");
-        startCombat()
+        var player = {
+            name: prompt("What are we going to call your fighter? (This is your chicken)"),
+            wins: 0,
+            health: 40,
+            healsRemaining: 2,
+            getDamage: function() {
+                   return Math.floor((Math.random() * 3) + 1);
+            },
+            heal: function() {
+                this.healsRemaining--;
+                return Math.floor((Math.random() * 10) + 1);
+            }
+        };
+
+        var grant = {
+            name: prompt("Who is your fighter's nemesis? (Keep in mind, this is also a chicken)"),
+            grant: 10,
+            getDamage: function() {
+                return Math.floor((Math.random() * 3) + 1);
+            }
+        };
+
+        startCombat(player, grant);
+        
     } else if (play !== "yes"){
         console.log("You know where to find me, if you change your mind.");
     }
@@ -37,32 +37,33 @@ function startGame() {
 
 
 function startCombat(player, grant) {
-
+    var game = true;
 
     while (game = true) {
 
         var nextRound = prompt("Would you like to attack, heal or chicken out?").toLowerCase();
 
         if (nextRound === "attack") {
-            player.health = player.health - player.getDamage();
-            grant.grant = grant.grant - grant.getDamage();
-
-            console.log(grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.");
+            player.health -= player.getDamage();
+            grant.grant -= grant.getDamage();
             console.log("You hit " + grant.name +"! They have " + grant.grant + " health remaining!");
-        } else if (nextRound !== "attack") {
+            console.log(grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.");
+
+        } else if (nextRound === "quit") {
             console.log("You were doing so well, though.");
             return;
-        }
 
-        if (nextRound === "heal" && player.healsRemaining > 0) {
-            player.health += player.heal();
-            player.healsRemaining -= 1;
-        } else if (nextRound === "heal" && player.healsRemaining <= 0) {
-            console.log("You've already used all your heals");
-            return;
-        }
-
-        if (grant.grant <= 0) {
+        } else if (nextRound === "heal") {
+            if (player.healsRemaining > 0) {
+                player.health += player.heal();
+                player.health -= player.getDamage();
+                console.log(grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.  Good thing you healed your little friend!");
+            } else if (player.healsRemaining <= 0) {
+                console.log("You've already used all your heals");
+                return;
+            }
+            
+        } if (grant.grant <= 0) {
             grant.grant = 10;
             player.wins += 1;
             console.log("Round " + player.wins + " won!");
@@ -74,7 +75,7 @@ function startCombat(player, grant) {
             break;
         }
 
-        if (player.wins === 5) {
+        if (player.wins >= 5) {
             console.log("Congratulations, " + player.name + "! " + grant.name + " is no more!");
             break;
         }
@@ -87,25 +88,20 @@ function startCombat(player, grant) {
 
 /*
 
-    Things that i think:
 
-        need to:
-            - "get to" the player and grant objects in startGame()
 
-        want to:
-            - create default player name
 
-    part 3 requirements:
 
-        //DONE  1. Each time the user is prompted, they have the choice either to attack, heal or quit.
-                        Grant always attacks.
-        //DONE  2. Do not allow heal if there are no heals remaining.
-        //DONE  3. Grant still regenerates to 10 when his health hits 0.  Every time this happens, add 1 to
-                        the user character's wins.
-        //DONE  4. When the user character's wins reaches 5, Grant is defeated and the game is over.
-        //DONE  5. When the user character's health reaches 0, the user is defeated and the game is over.
-        //DONE  6. Continue to log health and wins at each iteration using the name entered by the user at
-                        the beginning, just as in previous parts of the lab
+
+
+
+
+
+
+
+
+
+
 
 
  */
