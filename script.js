52376d44ca842da4f3382cb6fe4e3059aee416a1
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-02 13:20:49 -0400

    Completed part four, except status bar

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 var player = {
-    name: prompt("What are we going to call your fighter? (This is your chicken)"),
+    name: "White Lightning",
     wins: 0,
     health: 40,
     healsRemaining: 2,
-    getDamage: function() {
-           return Math.floor((Math.random() * 3) + 1);
+    gameDialog: "",
+    getDamage: function () {
+        return Math.floor((Math.random() * 3) + 1);
     },
-    heal: function() {
+    heal: function () {
         this.healsRemaining--;
         return Math.floor((Math.random() * 10) + 1);
     }
 };
 
 var grant = {
-    name: prompt("Who is your fighter's nemesis? (Keep in mind, this is also a chicken)"),
+    name: "Big Red",
     grant: 10,
-    getDamage: function() {
+    getDamage: function () {
         return Math.floor((Math.random() * 3) + 1);
     }
 };
@@ -26,46 +27,99 @@ function startCombat(choice) {
     var game = true;
 
 
-    var nextRound = choice;
-
-    if (nextRound === "attack") {
+    if (choice === "attack") {
         player.health -= player.getDamage();
         grant.grant -= grant.getDamage();
-        console.log("You hit " + grant.name +"! They have " + grant.grant + " health remaining!");
-        console.log(grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.");
+        player.gameDialog = "Your " + player.name + " jabbed " + grant.name + " and now they only have  " + grant.grant + " left " + grant.name + " pecked " + player.name + " and you have " + player.health + " health remaining.";
 
-    } else if (nextRound === "quit") {
-        console.log("You were doing so well, though.");
-        return;
+    } else if (choice === "quit") {
+        player.gameDialog = "You were doing so well, though.";
+        endGame();
 
-    } else if (nextRound === "heal") {
+    } else if (choice === "heal") {
         if (player.healsRemaining > 0) {
             player.health += player.heal();
             player.health -= player.getDamage();
-            console.log(grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.  Good thing you healed your little friend!");
+            player.gameDialog = grant.name + " pecked " + player.name + ". You have " + player.health + " health remaining.  Good thing you healed your little friend!";
         } else if (player.healsRemaining <= 0) {
-            console.log("You've already used all your heals");
-            nextRound = "attack";
+            player.gameDialog = "You've already used all your heals";
+            choice = "attack";
         }
-        
-    } if (grant.grant <= 0) {
+
+    }
+    if (grant.grant <= 0) {
         grant.grant = 10;
         player.wins += 1;
-        console.log("Round " + player.wins + " won!");
+        player.gameDialog = "Round " + player.wins + " won!";
     }
 
     if (player.health <= 0) {
         game = false;
-        console.log(grant.name + " owned you...better luck next time");
-        
+        player.gameDialog = grant.name + " owned you...better luck next time";
+
     }
 
     if (player.wins >= 5) {
-        console.log("Congratulations, " + player.name + "! " + grant.name + " is no more!");
-        
+        player.gameDialog = "Congratulations, " + player.name + "! " + grant.name + " is no more!";
+
     }
+
+    if (game === false) {
+        endGame();
+    }
+    updatePlayerInfo();
+    updateGrantInfo();
+    updateScore();
     
 }
+
+function updatePlayerInfo() {
+    var playerName = document.getElementById("playerName");
+    var healsRemaining = document.getElementById("healsRemaining");
+    var playerHealth = document.getElementById("playerHealth");
+    playerName.innerText = player.name;
+    healsRemaining.innerText = "Unused heals: " + player.healsRemaining;
+    playerHealth.innerText = player.name + " health: " + player.health; 
+
+}
+
+function updateGrantInfo() {
+    var grantName = document.getElementById("grantName");
+    var grantHealth = document.getElementById("grantHealth");
+    grantName.innerText = grant.name;
+    grantHealth.innerText = grant.name + " health: " + grant.grant;
+
+}
+
+function updateScore() {
+    var roundsWon = document.getElementById("roundsWon");
+    var gameDialog = document.getElementById("gameDialog");
+    roundsWon.innerText = "Rounds won: " + player.wins;
+    gameDialog.innerText = player.gameDialog;
+
+}
+
+function startGame() {
+    player.name = prompt("What are we going to call your fighter? (This is your chicken)");
+    grant.name = prompt("Who is your fighter's nemesis? (Keep in mind, this is also a chicken)");
+    updateScore();
+    updatePlayerInfo();
+    updateGrantInfo();
+    var actionButtons = document.querySelectorAll(".actionButtons")[0];
+    actionButtons.style.display = "flex";
+    
+}
+
+function endGame() {
+    var actionButtons = document.querySelectorAll(".actionButtons")[0];
+    actionButtons.style.display = "none";
+}
+
+
+
+
+
+
 
 
 
